f2d332d23f6c21f71cc1edb71c085b14c3d9b1ce
Kael Shipman <kael.shipman@cfxtrading.com>
2017-09-01 16:33:16 -0500

    Added extension of ErrorHandlerTrait to allow adding JsonApiErrors as errors

---
+++ src/ErrorHandlerTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace KS\JsonApi;
+
+trait ErrorHandlerTrait {
+    use \KS\ErrorHandlerTrait;
+
+    public function setError(string $field, string $which=null, ErrorInterface $error) {
+        if (!array_key_exists($field, $this->errors)) $this->errors[$field] = array();
+        if ($which) $this->errors[$field][$which] = $error;
+        else $this->errors[$field][] = $error;
+        return $this;
+    }
+}
+
