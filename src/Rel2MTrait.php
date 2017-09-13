f5055f2073073fa20e0e979c0c33f1d2450ff309
Kael Shipman <kael.shipman@cfxtrading.com>
2017-09-13 15:05:55 -0500

    Modified relationship trait for php 5.4

--- src/Rel2MTrait.php
+++ src/Rel2MTrait.php
@@ -2,18 +2,18 @@
 namespace KS\JsonApi;
 
 trait Rel2MTrait {
-    protected function add2MRel(string $name, BaseResourceInterface $resource) {
+    protected function add2MRel($name, BaseResourceInterface $resource) {
         if (!$this->has2MRel($resource)) $this->relationships[$name]->getData()[] = $resource;
         return $this;
     }
-    protected function has2MRel(string $name, BaseResourceInterface $resource=null) {
+    protected function has2MRel($name, BaseResourceInterface $resource=null) {
         if (!$resource || !$this->relationships[$name]->getData()) return false;
         foreach($this->relationships[$name]->getData() as $test) {
             if ($test->getId() == $resource->getId() && $test->getResourceType() == $resource->getResourceType()) return true;
         }
         return false;
     }
-    protected function remove2MRel(string $name, BaseResourceInterface $resource) {
+    protected function remove2MRel($name, BaseResourceInterface $resource) {
         if (!$this->relationships[$name]->getData()) return;
         foreach($this->relationships[$name]->getData() as $k => $test) {
             if ($test->getId() == $resource->getId() && $test->getResourceType() == $resource->getResourceType()) {
