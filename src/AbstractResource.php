dfc911ba424f15ece037f8fee4ebdc9be447b4ef
Kael Shipman <kael.shipman@cfxtrading.com>
2019-04-17 15:07:12 -0500

    Fixed some issues with fromResource method and change tracking

--- src/AbstractResource.php
+++ src/AbstractResource.php
@@ -220,6 +220,9 @@ abstract class AbstractResource implements ResourceInterface {
                 $initialState['attributes'][$name] = $src->initialState['attributes'][$name];
                 if (array_key_exists($name, $src->changes['attributes'])) {
                     $changes['attributes'][$name] = $src->changes['attributes'][$name];
+                } elseif (array_key_exists($name, $targ->changes["attributes"])) {
+                    var_dump("Unsetting changes for attr $name");
+                    unset($targ->changes["attributes"][$name]);
                 }
             }
         }
@@ -229,6 +232,9 @@ abstract class AbstractResource implements ResourceInterface {
                 $initialState['relationships'][$name] = $src->initialState['relationships'][$name];
                 if (array_key_exists($name, $src->changes['relationships'])) {
                     $changes['relationships'][$name] = $src->changes['relationships'][$name];
+                } elseif (array_key_exists($name, $targ->changes["relationships"])) {
+                    var_dump("Unsetting changes for rel $name");
+                    unset($targ->changes["relationships"][$name]);
                 }
             }
 
@@ -237,8 +243,8 @@ abstract class AbstractResource implements ResourceInterface {
             }
         }
 
-        $targ->internalUpdateFromData($data);
         $targ->initialState = array_replace_recursive($targ->initialState, $initialState);
+        $targ->internalUpdateFromData($data);
         $targ->changes = array_replace_recursive($targ->changes, $changes);
         $targ->initialized = $src->initialized;
 
